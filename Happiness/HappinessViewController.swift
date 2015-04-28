e3f908d75af96c03fdcf832771ed36545391947a
Scott Love <scottlove@unboxedtechnology.com>
2015-04-28 08:54:34 -0400

    Face is drawing and Scaling

---
+++ Happiness/HappinessViewController.swift
@@ -0,0 +1,14 @@
+//
+//  HappinessViewController.swift
+//  Happiness
+//
+//  Created by Scott Love on 4/25/15.
+//  Copyright (c) 2015 Scott Love. All rights reserved.
+//
+
+import UIKit
+
+class HappinessViewController: UIViewController {
+
+
+}
